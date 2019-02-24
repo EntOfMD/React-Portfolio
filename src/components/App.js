4d161935963a330bfc17e23163b593cdfdade2a6
Ashell Fernando <ashellz2010@gmail.com>
2019-02-24 13:47:46 -0500

    more cleaning up

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import '../css/App.css';
 
-class App extends React.Component {
+class App extends Component {
     render() {
-        return <h1>Hello</h1>;
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
     }
 }
 
