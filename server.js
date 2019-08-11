aedb52724c447dbe527deca636ec379f4b370776
Ashell Fernando <ashellz2010@gmail.com>
2019-08-10 21:55:45 -0400

    restarted from scratch.. added express+CRA

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(
+    helmet({
+        noCache: true,
+        permittedCrossDomainPolicies: true
+    })
+);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+} else if (app.get('env') === 'development') {
+    app.use(logger('dev'));
+}
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+    console.log(`App listening on http://localhost:${PORT}`);
+});
